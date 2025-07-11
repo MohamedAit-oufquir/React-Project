faa1ab9c12fdb3e4672e7d007804ea92cb474662
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2025-07-11 16:03:53 -0400

    fix mist

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Link } from "react-router-dom";
-import "./css/index.css";
+import './css/index.css';
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
--- src/services/Api.js
+++ src/services/Api.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 
 
 export const getPopularMovies = async () => {
-  console.log(`${BASE_URL}/movie/popular?api_key=${API_KEY}`,"hello")
+  console.log(`${BASE_URL}/movie/popular?api_key=${API_KEY}`,)
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
   return data.results;
